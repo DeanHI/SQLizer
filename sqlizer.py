5317060de4346ce8392afa2e6eabdfd5388e7ef8
DeanHI <49130581+DeanHI@users.noreply.github.com>
2019-05-02 02:56:21 +0300

    Update sqlizer.py

--- sqlizer.py
+++ sqlizer.py
@@ -3,44 +3,28 @@ import sqlite3
 from flask import Flask, render_template
 from schema_create import create_store_schema, insert_initial_values
 
+app = Flask(__name__)
 
-def main():
+@app.route('/') 
+@app.route('/home')
+def table_present():
 	sqlite_file = 'inventory_schema'	
 	con = sqlite3.connect(sqlite_file)
 	c = con.cursor()
+	col_list = []
+	val_list = []
+	c.execute('Select * from vacuumcleaners')
+	for i in c.fetchall():
+		val_list.append(i)
+	for j in c.description:
+		col_list.append(j[0])
+	return render_template('home.html', val_list=val_list, col_list=col_list)
 	
-	while True:
-		user_input = input("execute SQL script:")
+	
 		
-		if user_input[0:6].lower() == 'select':
-			c.execute(user_input)
-			print(c.fetchall())
-		elif user_input[0].lower() == 'q':
-			return
-		else:
-			c.execute(user_input)
-			con.commit()
-
-	con.close()
 
 
 
 if __name__ == "__main__":
-	main()
+	app.run(debug = True)
 	
-
-
-
-
-
-
-# @app.route('/') #remember this because this what comes after local host, which 127.0.0.1:whatever port(5000 default), then anything after / is another page
-# @app.route('/home')
-# def printme(): 
-	# return render_template ('home.html')
-
-
-#if __name__ == "__main__":
-	#with sqlite3.connect('inventory_schema') as  
-	#app.run(port = 443, debug = True)
-	#intro_print()
